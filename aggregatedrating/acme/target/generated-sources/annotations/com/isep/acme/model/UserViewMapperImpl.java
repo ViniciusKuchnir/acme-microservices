ee401a46c166e12bb18f54862335213d9524876a
Vinícius Kuchnir <vina200902@gmail.com>
2023-11-29 15:20:16 +0000

    Adição de arquivos de target ao projeto

---
+++ aggregatedrating/acme/target/generated-sources/annotations/com/isep/acme/model/UserViewMapperImpl.java
@@ -0,0 +1,30 @@
+package com.isep.acme.model;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-29T12:39:02+0000",
+    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
+)
+@Component
+public class UserViewMapperImpl extends UserViewMapper {
+
+    @Override
+    public UserView toUserView(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserView userView = new UserView();
+
+        userView.setFullName( user.getFullName() );
+        if ( user.getUserId() != null ) {
+            userView.setUserId( String.valueOf( user.getUserId() ) );
+        }
+        userView.setUsername( user.getUsername() );
+
+        return userView;
+    }
+}
